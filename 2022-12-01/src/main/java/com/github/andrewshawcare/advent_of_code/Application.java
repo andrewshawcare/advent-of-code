ae72366a7a6030a200301c9d669f6a0c50777bdf
Andrew Shaw Care <andrewshawcare@gmail.com>
2022-12-23 14:59:37 -0500

    Added initial build and publish step

---
+++ 2022-12-01/src/main/java/com/github/andrewshawcare/advent_of_code/Application.java
@@ -0,0 +1,23 @@
+package com.github.andrewshawcare.advent_of_code;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+        return args -> {
+            for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
+                System.out.println(beanDefinitionName);
+            }
+        };
+    }
+}
